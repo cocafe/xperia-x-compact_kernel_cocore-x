df0fa6f61bb719c0290ecd24acd2b52e6c8b16ad
Ji Huang <cocafehj@gmail.com>
2017-08-11 16:56:53 +0800

    block: sio: tune to prefer reading
    Signed-off-by: Ji Huang <cocafehj@gmail.com>

--- block/sio-iosched.c
+++ block/sio-iosched.c
@@ -29,8 +29,8 @@ static const int sync_write_expire = 2 * HZ;        /* max time before a sync wr
 static const int async_read_expire  =  HZ / 8;        /* ditto for async, these limits are SOFT! */
 static const int async_write_expire = 2 * HZ;        /* ditto for async, these limits are SOFT! */
 
-static const int writes_starved = 1;                /* max times reads can starve a write */
-static const int fifo_batch     = 1;                /* # of sequential requests treated as one
+static const int writes_starved = 8;                /* max times reads can starve a write */
+static const int fifo_batch     = 4;                /* # of sequential requests treated as one
                                                    by the above parameters. For throughput. */
 
 /* Elevator data */
