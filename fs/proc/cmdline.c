e589e91957a1e391e7f517e6fff6e5ecd3c168c9
Ji Huang <cocafehj@gmail.com>
2024-11-22 18:56:41 +0800

    cmdline: hack to bypass charging boot
    Signed-off-by: Ji Huang <cocafehj@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -49,6 +49,17 @@ static int __init proc_cmdline_init(void)
 		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
 	}
 
+	offset_addr = strstr(cmd, "startup=");
+	if (offset_addr) {
+		const char *magic = "0x00000001"; // BIT 1 = POWER KEY | BIT 3 = CHARGING
+		char *s = &offset_addr[8]; // locate 0x00000000
+		size_t i;
+
+		for (i = 0; i < strlen(magic); i++) {
+			s[i] = magic[i];
+		}
+	}
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
